3fef208689e8a65c265a12660ecdf7152e78b46a
yemiwebby <yemiwebby@gmail.com>
2020-06-26 23:03:13 +0100

    new commits

--- app/Http/Controllers/API/CEOController.php
+++ app/Http/Controllers/API/CEOController.php
@@ -10,4 +10,81 @@ use Illuminate\Support\Facades\Validator;
 
 class CEOController extends Controller
 {
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'company_name' => 'required|max:255',
+            'year' => 'required|max:255',
+            'company_headquarters' => 'required|max:255',
+            'what_company_does' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+
+        $ceo = CEO::create($data);
+
+        return response([ 'ceo' => new CEOResource($ceo), 'message' => 'Created successfully'], 201);
+    }
+
+
+    public function index()
+    {
+        $ceos = CEO::all();
+
+        return response([ 'ceos' => CEOResource::collection($ceos), 'message' => 'Retrieved successfully'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CEO  $ceo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CEO $ceo)
+    {
+        return response([ 'ceo' => new CEOResource($ceo), 'message' => 'Retrieved successfully'], 200);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CEO  $ceo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CEO $ceo)
+    {
+
+        $ceo->update($request->all());
+
+        return response([ 'ceo' => new CEOResource($ceo), 'message' => 'Updated successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\CEO $ceo
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
+
+
+    public function destroy(CEO $ceo)
+    {
+        $ceo->delete();
+
+        return response(['message' => 'Deleted'], 204);
+    }
 }
